86a35fbc2c535044e079bace092bd6584936c9c5
OMAGE MICHEAL BRIGHT <micheal.omage@gmail.com>
2023-03-09 10:23:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,48 @@
 <?php 
-    $id = $_POST["id"];
-    $ticket = 0;
-    $type = "";
-    $size = 0;
-    $symbol = "";
-    $price = 0;
-    $sl = 0;
-    $tp = 0;
-    $comment = "";   
+     
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        // Access GET request parameters using $_GET superglobal
-        $name = $_GET['name'];
-        $age = $_GET['age'];
-    
-        // Process GET request
-        //echo "Hello {$name}, you are {$age} years old!";
-        // Get the query string from the request URL
-        $query_string = $_SERVER['QUERY_STRING'];
         
-        // Parse the query string into an array of key-value pairs
-        parse_str($query_string, $queries);
+        if(isset($_GET["id"])){
+            $id = $_GET["id"];
+            //echo "Hello {$id}";
+            $filename = "{$id}.txt";
+            if (file_exists($filename)) {
+                $file_contents = file_get_contents($filename);
+                echo $file_contents;
+            } else {
+                echo "NA";
+            }
+            
+        }else{
+            echo "NA";
+        }
+        echo "<br />Get request loading ";
         
-        // Output the array of queries
-        print_r($queries);
-        echo "Get request loading {$name} <br />";
         
     }
     else if($_SERVER['REQUEST_METHOD'] === 'POST'){
         // Create a PHP array containing some data
         $id = $_POST["id"];
-        $ticket = 0;
-        $type = "";
-        $size = 0;
-        $symbol = "";
-        $price = 0;
-        $sl = 0;
-        $tp = 0;
-        $comment = "";
-        echo $id;
+        $ticket = $_POST["Ticket"];
+        $symbol = $_POST["Order_name"];
+        $type = $_POST["Order_type"];
+        $size = $_POST["Lot_size"];
+        $price = $_POST["Price"];
+        $sl = $_POST["Sl"];
+        $tp = $_POST["Tp"];
+        $comment = $_POST["Order_comment"];
+        
+        $data = $id+"-"+$ticket+"-"+$symbol+"-"+$type+"-"+$size+"-"+$price+$sl+$tp+$comment;
+        // Open a file for writing
+        $file = fopen("{$id}.txt", "w");
+        
+        // Write a string to the file
+        fwrite($file, $data);
+        
+        // Close the file
+        fclose($file);
+        
+        echo $id + " data uploaded successfully";
         
     }else{
         echo "These method is not accepted for program";
